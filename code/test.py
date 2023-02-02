7c42fc7c3c4d2f9538b23af5cd7e1bbb2cb85d3a
juicyjung <travelandi01@gmail.com>
2023-02-03 07:41:36 +0900

    ETL Process

---
+++ code/test.py
@@ -0,0 +1,62 @@
+import openai
+
+import math
+import time
+
+
+
+with open('openai_key.txt') as f:
+  API_TOKEN = f.readline()
+
+openai.api_key= API_TOKEN
+
+
+extract_from = "https://learn.microsoft.com/en-us/"
+extract_what = "every word in tag <table>"
+transform = "if that word is /^l.*y$/"
+load = "Azure"
+
+
+
+p = f"""
+Can you make ETL Python code?
+
+Extract from:
+{extract_from}
+
+Extract what:
+{extract_what}
+
+Transform:
+{transform}
+
+Load:
+{load}
+"""
+
+
+
+start = time.time()
+
+
+
+  
+response = openai.Completion.create(
+  engine="text-davinci-003",
+  prompt = p,
+  temperature=0.3,
+  max_tokens=256,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+output = response['choices'][0]['text']
+
+print(output)
+
+
+math.factorial(100000)
+end = time.time()
+
+print(f"\n\n{end - start:.5f} sec")
